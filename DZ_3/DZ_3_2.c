6fcff457b4f541c4239d40d879552dfe6f7e92d9
dubikita5298-max <dubikita5298@gmail.com>
2025-12-15 19:49:18 +0700

    Add files via upload

---
+++ DZ_3/DZ_3_2.c
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>
+int main() {
+		float x = 5.0;
+		printf("x = %f, ", x);
+		float y = 6.0;
+		printf("y = %f\n", y);
+		float* xp =  &y;//Заменил указатель на адрес переменной y
+		float* yp = &y;
+		printf("Result: %f\n", *xp + *yp);
+	return 0;
+}
