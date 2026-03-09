5128766fbe0b1cfc454f6a9bae7101d618634344
dubikita5298-max <dubikita5298@gmail.com>
2026-03-09 17:30:55 +0700

    Add files via upload

---
+++ DZ_7/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void add();//Прототип ф-ии сложения
+void sub();//Прототип ф-ии вычитания
+void mul();//Прототип ф-ии умножения
+void div();//Прототип ф-ии деления
+
+#endif
